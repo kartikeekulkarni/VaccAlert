6e2d79c738304d74fcfaa50c59fd11fbbc9e2868
aniketrchate <anikrchate@gmail.com>
2024-08-20 23:27:46 +0530

    Add State.cs

---
+++ DotNetWebAPI/vaccalertDotnet/Models/Parent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccalertDotnet.Models
+{
+    public partial class Parent
+    {
+        public Parent()
+        {
+            Bookings = new HashSet<Booking>();
+            Children = new HashSet<Child>();
+        }
+
+        public int Pid { get; set; }
+        public int? Uid { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public string? Contact { get; set; }
+        public string? Email { get; set; }
+        public int? Paddress { get; set; }
+
+        public virtual Address? PaddressNavigation { get; set; }
+        public virtual User? UidNavigation { get; set; }
+        public virtual ICollection<Booking>? Bookings { get; set; }
+        public virtual ICollection<Child>? Children { get; set; }
+    }
+}
---
+++ DotNetWebAPI/vaccalertDotnet/Models/VaccineStock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccalertDotnet.Models
+{
+    public partial class VaccineStock
+    {
+        public int Vsid { get; set; }
+        public int? Hid { get; set; }
+        public int? Vid { get; set; }
+        public int? Stock { get; set; }
+
+        public virtual Hospital? HidNavigation { get; set; }
+        public virtual Vaccine? VidNavigation { get; set; }
+    }
+}
