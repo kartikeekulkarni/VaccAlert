d5a1396d3d16b77a8d3f485b3ca66fd6a4116179
aniketrchate <anikrchate@gmail.com>
2024-08-20 23:27:44 +0530

    Add Hospital.cs

---
+++ DotNetWebAPI/vaccalertDotnet/Models/Hospital.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccalertDotnet.Models
+{
+    public partial class Hospital
+    {
+        public Hospital()
+        {
+            Bookings = new HashSet<Booking>();
+            VaccineStocks = new HashSet<VaccineStock>();
+        }
+
+        public int Hid { get; set; }
+        public int? Uid { get; set; }
+        public string? Hname { get; set; }
+        public string? Email { get; set; }
+        public string? Url { get; set; }
+        public int? Hadress { get; set; }
+        public string? Contact { get; set; }
+        public bool? Status { get; set; }
+
+        public virtual Address? HadressNavigation { get; set; }
+        public virtual User? UidNavigation { get; set; }
+        public virtual ICollection<Booking>? Bookings { get; set; }
+        public virtual ICollection<VaccineStock>? VaccineStocks { get; set; }
+    }
+}
