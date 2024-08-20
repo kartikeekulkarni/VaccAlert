9c3af8e903f1ee235786633d116d441ad54e96f3
aniketrchate <anikrchate@gmail.com>
2024-08-20 23:27:43 +0530

    Add Child.cs

---
+++ DotNetWebAPI/vaccalertDotnet/Models/Child.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccalertDotnet.Models
+{
+    public partial class Child
+    {
+        public Child()
+        {
+            Bookings = new HashSet<Booking>();
+        }
+
+        public int Cid { get; set; }
+        public int? Pid { get; set; }
+        public string? Cname { get; set; }
+        public DateOnly? Dob { get; set; }
+        public string? Gender { get; set; }
+
+        public virtual Parent? PidNavigation { get; set; }
+        public virtual ICollection<Booking>? Bookings { get; set; }
+    }
+}
