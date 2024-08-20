f4b896a57a06e63c36a4f2f67cd2a35facdc9b43
aniketrchate <anikrchate@gmail.com>
2024-08-20 23:27:43 +0530

    Add City.cs

---
+++ DotNetWebAPI/vaccalertDotnet/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccalertDotnet.Models
+{
+    public partial class City
+    {
+        public City()
+        {
+            Addresses = new HashSet<Address>();
+        }
+
+        public int Cityid { get; set; }
+        public string? Cityname { get; set; }
+        public int? Did { get; set; }
+
+        public virtual District? DidNavigation { get; set; }
+        public virtual ICollection<Address>? Addresses { get; set; }
+    }
+}
